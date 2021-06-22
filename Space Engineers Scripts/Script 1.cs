7bb5c0aee7a6928b18ade5ebd917f8d4304b2a2c
Александр <Samulabars@Gmail.com>
2021-06-23 03:34:23 +0500

    Написаны старт и стоп. Добавлены основной ротор и буры.

--- Space Engineers Scripts/Script 1.cs
+++ Space Engineers Scripts/Script 1.cs
@@ -18,14 +18,19 @@ namespace Script1
     {
         //------------BEGIN--------------
 
+        List<IMyTerminalBlock> rotor = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> drills = new List<IMyTerminalBlock>();
         List<IMyTerminalBlock> hinges_d = new List<IMyTerminalBlock>();
         List<IMyTerminalBlock> hinges_s1 = new List<IMyTerminalBlock>();
         List<IMyTerminalBlock> hinges_s2 = new List<IMyTerminalBlock>();
         List<IMyTerminalBlock> hinges_s3 = new List<IMyTerminalBlock>();
+
         public Program()
         {
             //Наполнение списков уникальными именами объектов из игры
 
+            GridTerminalSystem.SearchBlocksOfName("[rotor]", rotor, b => b is IMyMotorStator);
+            GridTerminalSystem.SearchBlocksOfName("[drill]", drills, b => b is IMyShipDrill);
             GridTerminalSystem.SearchBlocksOfName("[hinge_d]", hinges_d, b => b is IMyMotorStator);
             GridTerminalSystem.SearchBlocksOfName("[hinge_s1]", hinges_s1, b => b is IMyMotorStator);
             GridTerminalSystem.SearchBlocksOfName("[hinge_s2]", hinges_s2, b => b is IMyMotorStator);
@@ -36,10 +41,10 @@ namespace Script1
         public void Main(string args)
         {
             switch (args)
-			{
+            {
                 case "stepside_go":   //Лапы в стороны
                     step_s++;
-                    for (int step_c = 0; step_c <181 & step_c < step_s; step_c++)   //Счетчик от 0 до 180 и математика шарниров
+                    for (int step_c = 0; step_c < 181 & step_c < step_s; step_c++)   //Счетчик от 0 до 180 и математика шарниров
                     {
                         int step_s1; int step_s2; int step_s3;
 
@@ -48,7 +53,7 @@ namespace Script1
                         step_s2 = step_s2 + step_c;   //-90 to 90
                         step_s3 = step_s3 + step_c / 2;   //0 to 90
 
-                        for (int i = 0; i< hinges_s1.Count; i++)   //Передача значений в шарниры
+                        for (int i = 0; i < hinges_s1.Count; i++)   //Передача значений в шарниры
                         {
                             var hinge_s1 = hinges_s1[i] as IMyMotorStator;
                             var hinge_s2 = hinges_s2[i] as IMyMotorStator;
@@ -88,12 +93,15 @@ namespace Script1
                 case "stepdown_go":   //Платформа вниз
                     step_d++;
                     for (int step_c = 0; step_d < 181 & step_c < step_d; step_c++) //Счетчик от 0 до 180
-                    { 
+                    {
                         for (int i = 0; i < hinges_d.Count; i++)   //Передача значений в шарниры
                         {
                             var hinge_d = hinges_d[i] as IMyMotorStator;
 
-                            hinge_d.SetValueFloat("UpperLimit", step_d);
+                            int step_d1; step_d1 = -90;
+                            step_d1 = step_d1 + step_c;
+
+                            hinge_d.SetValueFloat("UpperLimit", step_d1);
                             hinge_d.TargetVelocityRPM = 0.01F;
                         };
                     };
@@ -114,21 +122,41 @@ namespace Script1
                     // Сделать таймер с циклом
                     break;
 
-                case "start":   //Старт скрипта
-                        // Сделать старт ротора и буров
+                case "start":   //Старт работы
+                    for (int i = 0; i < hinges_s1.Count; i++)
+                    {
+                        var rotor_1 = rotor[i] as IMyMotorStator;
+                        var drills_1 = drills[i] as IMyMotorStator;
+
+                        rotor_1.ApplyAction("OnOff_On");
+                        rotor_1.SetValueFloat("Torque", 100);
+                        rotor_1.SetValueFloat("BrakingTorque", 0);
+                        rotor_1.TargetVelocityRPM = 0.01F;
+                        drills_1.ApplyAction("OnOff_On");
+                    };
                     break;
 
-                case "stop":   //Остановка скрипта
-                    // Сделать остановку ротора и буров.
+                case "stop":   //Остановка работы
+                    for (int i = 0; i < hinges_s1.Count; i++)
+                    {
+                        var rotor_1 = rotor[i] as IMyMotorStator;
+                        var drills_1 = drills[i] as IMyMotorStator;
+
+                        rotor_1.ApplyAction("OnOff_Off");
+                        rotor_1.SetValueFloat("Torque", 0);
+                        rotor_1.SetValueFloat("BrakingTorque", 100);
+                        rotor_1.TargetVelocityRPM = 0;
+                        drills_1.ApplyAction("OnOff_Off");
+                    };
                     break;
                 default:   //Любой иной аргумен вызывает остановку скрипта
                     break;
-            }         
+            }
         }
 
         public void Save()
         {
-        
+
         }
 
         //------------END--------------
